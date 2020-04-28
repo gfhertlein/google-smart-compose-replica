b1884543c00e0e93b35c58801a84cad03c291e83
Fred Hertlein <gfhertlein@users.noreply.github.com>
2020-04-28 00:38:06 -0700

    Attempted addition of simple calculation tf model (failed)

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Chatbox.css';
-import { WordsList } from '../../dictionaries/words';
+import * as tf from '@tensorflow/tfjs';
+//let model;
 
-const MAX_NUMBER_OF_LETTERS_CHECKED = 4;
-const MIN_CHAR_DIFFERENCE_BETWEEN_WORD_AND_ABBR = 2;
+
+//async function runthis() {
+//    const MODEL_URL = '../../../src/model/model.json';
+//    alert(ls ../);
+//    const model = await tf.loadLayersModel(MODEL_URL);
+//    console.log(model.summary());
+//    const input = tf.tensor2d([10.0], [1,1]);
+//    const result = model.predict(input);
+//     alert(result);
+//};
+//
+//runthis();
+
+
+//// Define a model for linear regression.
+//const model = tf.sequential();
+//model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+//
+//// Prepare the model for training: Specify the loss and the optimizer.
+//model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+//
+//// Generate some synthetic data for training.
+//const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+//const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+//
+//// Train the model using the data.
+//model.fit(xs, ys).then(() => {
+//  // Use the model to do inference on a data point the model hasn't seen before:
+//  alert(model.predict(tf.tensor2d([5], [1, 1])));
+//});
+
+
+
 
 const setCursor = (wordLength) => {
   const el = document.getElementById("box");
@@ -20,10 +52,7 @@ class ChatBox extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      matchedTerm: '',
-      matchedWord: false,
-      html: '',
-      words: WordsList.getWordsList
+      html: ''
     }
     this.chatBox = React.createRef();
   }
@@ -34,29 +63,14 @@ class ChatBox extends React.PureComponent {
     });
   }
 
-  checkIfLastWordMatchesExistingWords (word)  {
-    const wordMatchesCases = {
-      2: () => this.state.words[word.slice(0, 2)] ? { word: this.state.words[word.slice(0, 2)], char: 2 } : {},
-      3: () => this.state.words[word.slice(0, 3)] ? { word: this.state.words[word.slice(0, 3)], char: 3 } : {},
-      4: () => this.state.words[word.slice(0, 4)] ? { word: this.state.words[word.slice(0, 4)], char: 4 } : {},
-      default: {}
-    };
-    return word && wordMatchesCases[word.length] ? wordMatchesCases[word.length]() : wordMatchesCases.default
-  }
-
   updateMessage = (e) => {
     this.autoCompleteWord(e);
-    if(e.which === 32 && e.type === 'keydown') {
-      this.learnNewWords();
-    }
   }
 
   triggerSuggestions = (e, text, char) => {
     const textBeforeUpdate = e.target.innerText;
     const autocompletion = this.state.words[text].slice(char, this.state.words[text].length);
     this.setState({
-      matchedTerm: autocompletion,
-      matchedWord: true,
       html: `${e.target.innerText}<span class="autocompletion-text">${autocompletion}</span>`
     });
     const newCursor = textBeforeUpdate.length;
@@ -68,119 +82,39 @@ class ChatBox extends React.PureComponent {
       return;
     }
 
-    if(!Object.keys(this.state.words).length) {
-      return;
-    }
-
-    if(e.which === 13 && this.state.matchedWord) {
+    if(e.which === 13) {
       this.setState({
         html: e.target.innerText,
-        matchedWord: false,
       });
-
+      //alert(e.target.innerText);
       ReactDOM.findDOMNode(this.chatBox.current).focus();
       const updatedCursor = e.target.innerText.length;
       setCursor(updatedCursor);
       e.preventDefault();
     }
-
-    if(e.which !== 13 && this.state.matchedWord) {
-      this.setState({
-        html: e.target.innerText.slice(0, -this.state.matchedTerm.length),
-        matchedTerm: '',
-        matchedWord: false
-      });
-      setCursor(this.state.html.length);
-    }
-
-    const inputtedText = e.target.innerText;
-    const inputtedArr = inputtedText.split(' ');
-    const lastWord = inputtedArr[inputtedArr.length - 1];
-    const wordIfMatched = lastWord ? this.checkIfLastWordMatchesExistingWords(lastWord) : {};
-
-    if (wordIfMatched.word) {
-      const noAutocompletionText = inputtedText.split(' ');
-      const matchText = noAutocompletionText[noAutocompletionText.length - 1].substring(0, wordIfMatched.char);
-      this.triggerSuggestions(e, matchText, wordIfMatched.char);
-    }
   }
-
-  learnNewWords = () => {
-    const fullText = this.chatBox.current.innerText;
-    const validText = fullText.split(' ').filter((word) => {
-      if(word.length >= MAX_NUMBER_OF_LETTERS_CHECKED) {
-        return word;
-      }
-    });
-
-    if(!validText.length) {
-      return;
-    }
-    
-    const existingWordsList = WordsList.getWordsList;
-    const tempWordsList = {};
-    validText.forEach((word) => {
-      const isWordAlreadyInList = (wordToCheck) => !!Object.values(WordsList.getWordsList).filter(word => word === wordToCheck).length
-      const isAbbrAlreadyInList = (abbrToCheck) => !!Object.keys(WordsList.getWordsList).filter(abbr => abbr === abbrToCheck).length;
-      const twoWordSubstring = word.substring(0, 2);
-      const threeWordSubstring = word.substring(0, 3);
-      const fourWordSubstring = word.substring(0, 4);
-      const isWordWithinMinLength = (substring) =>  word.length - substring.length >= MIN_CHAR_DIFFERENCE_BETWEEN_WORD_AND_ABBR;
-
-      if(!existingWordsList[twoWordSubstring] && isWordWithinMinLength(twoWordSubstring) && !isWordAlreadyInList(word) && !isAbbrAlreadyInList(twoWordSubstring)) {
-        tempWordsList[twoWordSubstring] = word;
-        return;
-      }
-
-      if(!existingWordsList[threeWordSubstring] && isWordWithinMinLength(threeWordSubstring) && !isWordAlreadyInList(word) && !isAbbrAlreadyInList(threeWordSubstring)) {
-        tempWordsList[threeWordSubstring] = word;
-        return;
-      }
-
-      if(!existingWordsList[fourWordSubstring] && isWordWithinMinLength(fourWordSubstring) && !isWordAlreadyInList(word) && !isAbbrAlreadyInList(fourWordSubstring)) {
-        tempWordsList[fourWordSubstring] = word;
-        return;
-      }
-    });
-
-    WordsList.setWordsList = tempWordsList;
-    this.setState({
-      ...this.state,
-      words: WordsList.getWordsList
-    })
+  
+  mod_predict = (e) => {
+      
   }
 
   render () {
-    const { words } = this.state;
-    const wordsAbbrList = Object.keys(words);
+    //const { words } = this.state;
+    //const wordsAbbrList = Object.keys(words);
     return (
       <div className="row">
         <div className="col-md-3">
-          {
-            wordsAbbrList.length ?<div className="wordsList">
-           <ul className="list-group">
-            <li className="list-group-item active">Learned words&nbsp; 
-              <span className="badge badge-warning badge-pill"> {wordsAbbrList.length}</span>
-            </li>
-            {
-              wordsAbbrList.map(abbr => {
-                return (<li className="list-group-item" key={abbr}>{ `${abbr} - ${this.state.words[abbr]}` }</li>)
-              })
-            }
-            </ul>
-          </div>  : null
-          }
         </div>
         <div className="card col-md-6">
         <h5 className="card-header">
-          Google smart compose replica
+          Simple Calculation
         </h5>
         <div className="card-body">
         <div className="ChatBox">
             <div className="ChatBoxMessage">
             <div className="input-group">
               <div className="input-group-prepend">
-                <span className="input-group-text">Message box</span>
+                <span className="input-group-text">Number to Calculate</span>
               </div>
               <div
                 ref={this.chatBox}
@@ -197,13 +131,7 @@ class ChatBox extends React.PureComponent {
           <div>
             <ul>
               <li>
-                To autocomplete an existing abbreviation from the list press ENTER.
-              </li>
-              <li>
-                Words must have a min length of 4 characters.
-              </li>
-              <li>
-                Words must have at least 2 characters between the abbreviation and the actual word.
+                To make calculation press ENTER.
               </li>
             </ul>
           </div>
